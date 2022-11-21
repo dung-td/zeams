9f32a64fdfb36730c1fb96bc61262cc8dd94e9d0
congdanh1252 <70893956+congdanh1252@users.noreply.github.com>
2022-11-21 16:42:09 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom"
 import { Provider } from "react-redux"
 import { store } from "./redux/store"
 
-ReactDOM.create(
+ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
